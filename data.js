bfd781642094a054084c60ecf6dc0e183a86c646
0ahmedfarouk <ahmedfarouk422009@gmail.com>
2025-09-23 04:02:58 +0300

    Update data.js

--- data.js
+++ data.js
@@ -1,14 +1,18 @@
-const lessons=[
-  {chapter:"Chapter 1",lessons:[
-    {title:"Chapter 1 - Lesson 1",images:["./images/chapter1/L1-1.jpg","./images/chapter1/L1-2.jpg"]},
-    {title:"Chapter 1 - Lesson 2",images:["./images/chapter1/L2-1.jpg","./images/chapter1/L2-2.jpg"]},
-    {title:"Chapter 1 - Lesson 3",images:["./images/chapter1/L3-1.jpg","./images/chapter1/L3-2.jpg"]},
-    {title:"Chapter 1 - Lesson 4",images:["./images/chapter1/L4-1.jpg","./images/chapter1/L4-2.jpg"]},
-    {title:"Chapter 1 - Lesson 5",images:["./images/chapter1/L5-1.jpg","./images/chapter1/L5-2.jpg"]},
-    {title:"Chapter 1 - Lesson 6",images:["./images/chapter1/L6-1.jpg","./images/chapter1/L6-2.jpg"]},
-    {title:"Chapter 1 - Lesson 7",images:["./images/chapter1/L7-1.jpg","./images/chapter1/L7-2.jpg"]},
-  ]},
-  {chapter:"Chapter 2",lessons:[
-    {title:"Chapter 2 - Lesson 1",images:["./images/chapter2/L1-1.jpg","./images/chapter2/L1-2.jpg"]},
-  ]},
+const lessons = [
+  { chapter: "Chapter 1", lessons: [
+      { title: "Chapter 1 - Lesson 1", images: ["images/chapter1/L1-1.webp","images/chapter1/L1-2.webp"] },
+      { title: "Chapter 1 - Lesson 2", images: ["images/chapter1/L2-1.webp","images/chapter1/L2-2.webp"] },
+      { title: "Chapter 1 - Lesson 3", images: ["images/chapter1/L3-1.webp","images/chapter1/L3-2.webp"] },
+      { title: "Chapter 1 - Lesson 4", images: ["images/chapter1/L4-1.webp","images/chapter1/L4-2.webp"] },
+      { title: "Chapter 1 - Lesson 5", images: ["images/chapter1/L5-1.webp","images/chapter1/L5-2.webp"] },
+      { title: "Chapter 1 - Lesson 6", images: ["images/chapter1/L6-1.webp","images/chapter1/L6-2.webp"] },
+      { title: "Chapter 1 - Lesson 7", images: ["images/chapter1/L7-1.webp","images/chapter1/L7-2.webp","images/chapter1/L7-3.webp","images/chapter1/L7-4.webp","images/chapter1/L7-5.webp"] },
+      { title: "Chapter 1 - Lesson 8", images: ["images/chapter1/L8-1.webp","images/chapter1/L8-2.webp"] },
+      { title: "Chapter 1 - Lesson 9", images: ["images/chapter1/L9-1.webp","images/chapter1/L9-2.webp"] },
+    ]
+  },
+  { chapter: "Chapter 2", lessons: [
+      { title: "Chapter 2 - Lesson 1", images: ["images/chapter2/L1-1.webp","images/chapter2/L1-2.webp"] },
+    ]
+  }
 ];
